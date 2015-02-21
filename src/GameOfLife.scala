50a2fe54579203ee677a3b27e7cb2f3b35036bce
josephyao <dermott@126.com>
2015-02-21 15:37:59 +0800

    Refactor to rules list. This refactoring may not be necessary if we say the rule for live cell will not change.

--- src/GameOfLife.scala
+++ src/GameOfLife.scala
@@ -7,11 +7,16 @@ class GameOfLife(val liveCells: List[(Int, Int)]) {
     val cellsHaveLiveNeighbours = directions.flatMap(
       direction => liveCells.map(cell => (cell._1 + direction._1, cell._2 + direction._2)))
 
-    val liveCellsAfterTick = cellsHaveLiveNeighbours.groupBy(cell => cell).
-        filter(cellWithLiveNeighbours =>
-                cellWithLiveNeighbours._2.length == 2 && liveCells.contains(cellWithLiveNeighbours._1) ||
-                cellWithLiveNeighbours._2.length == 3).
-        keys.toList
+    def liveRuleWithTwoLiveNeighbours(cellWithLiveNeighbours: ((Int, Int), List[(Int, Int)])) =
+      cellWithLiveNeighbours._2.length == 2 && liveCells.contains(cellWithLiveNeighbours._1)
+    def liveRuleWithThreeLiveNeighbours(cellWithLiveNeighbours: ((Int, Int), List[(Int, Int)])) =
+      cellWithLiveNeighbours._2.length == 3
+
+    val rules = List(liveRuleWithTwoLiveNeighbours(_), liveRuleWithThreeLiveNeighbours(_))
+
+    val liveCellsAfterTick = rules.flatMap(
+      rule => cellsHaveLiveNeighbours.groupBy(cell => cell).filter(rule)).
+      map(_._1)
 
     return new GameOfLife(liveCellsAfterTick)
   }
